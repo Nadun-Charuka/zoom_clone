a87e4a609c3e7f3a2f05e637888ca5e9f134d641
Nadun Charuka <nadun.charuka2016@gmail.com>
2025-08-20 12:22:12 +0530

    setup done

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color appbarColor = Color(0xfff0f4f7);
+const Color bodyColor = Color(0xfffcfefd);
+const Color iconBackground = Color(0xff34353d);
+const Color bottomSheetColor = Color(0xff222222);
